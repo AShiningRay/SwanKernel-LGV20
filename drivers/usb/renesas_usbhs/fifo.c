46b9a2db78e91ee6ed734367be93ce4fbd8b8408
Greg Kroah-Hartman <gregkh@google.com>
2019-07-23 09:30:53 +0200

    Revert "usb: renesas_usbhs: add a workaround for a race condition of workqueue"
    This reverts commit 40b0b682c4aa96b44439a5b47472fc95e4ecb174.
    
    It too did not need to be in 3.18.y :(
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ic8559b44cd0e493cbd09d3e4aefa7afbc35c7cdc

--- drivers/usb/renesas_usbhs/fifo.c
+++ drivers/usb/renesas_usbhs/fifo.c
@@ -829,8 +829,9 @@ static int __usbhsf_dma_map_ctrl(struct usbhs_pkt *pkt, int map)
 }
 
 static void usbhsf_dma_complete(void *arg);
-static void usbhsf_dma_xfer_preparing(struct usbhs_pkt *pkt)
+static void xfer_work(struct work_struct *work)
 {
+	struct usbhs_pkt *pkt = container_of(work, struct usbhs_pkt, work);
 	struct usbhs_pipe *pipe = pkt->pipe;
 	struct usbhs_fifo *fifo = usbhs_pipe_to_fifo(pipe);
 	struct usbhs_priv *priv = usbhs_pipe_to_priv(pipe);
@@ -838,10 +839,12 @@ static void usbhsf_dma_xfer_preparing(struct usbhs_pkt *pkt)
 	struct dma_chan *chan = usbhsf_dma_chan_get(fifo, pkt);
 	struct device *dev = usbhs_priv_to_dev(priv);
 	enum dma_transfer_direction dir;
+	unsigned long flags;
 
+	usbhs_lock(priv, flags);
 	fifo = usbhs_pipe_to_fifo(pipe);
 	if (!fifo)
-		return;
+		goto xfer_work_end;
 
 	chan = usbhsf_dma_chan_get(fifo, pkt);
 	dir = usbhs_pipe_is_dir_in(pipe) ? DMA_DEV_TO_MEM : DMA_MEM_TO_DEV;
@@ -850,14 +853,14 @@ static void usbhsf_dma_xfer_preparing(struct usbhs_pkt *pkt)
 					pkt->trans, dir,
 					DMA_PREP_INTERRUPT | DMA_CTRL_ACK);
 	if (!desc)
-		return;
+		goto xfer_work_end;
 
 	desc->callback		= usbhsf_dma_complete;
 	desc->callback_param	= pipe;
 
 	if (dmaengine_submit(desc) < 0) {
 		dev_err(dev, "Failed to submit dma descriptor\n");
-		return;
+		goto xfer_work_end;
 	}
 
 	dev_dbg(dev, "  %s %d (%d/ %d)\n",
@@ -866,17 +869,8 @@ static void usbhsf_dma_xfer_preparing(struct usbhs_pkt *pkt)
 	usbhs_pipe_running(pipe, 1);
 	usbhs_pipe_set_trans_count_if_bulk(pipe, pkt->trans);
 	usbhs_pipe_enable(pipe);
-}
 
-static void xfer_work(struct work_struct *work)
-{
-	struct usbhs_pkt *pkt = container_of(work, struct usbhs_pkt, work);
-	struct usbhs_pipe *pipe = pkt->pipe;
-	struct usbhs_priv *priv = usbhs_pipe_to_priv(pipe);
-	unsigned long flags;
-
-	usbhs_lock(priv, flags);
-	usbhsf_dma_xfer_preparing(pkt);
+xfer_work_end:
 	usbhs_unlock(priv, flags);
 }
 
@@ -924,13 +918,8 @@ static int usbhsf_dma_prepare_push(struct usbhs_pkt *pkt, int *is_done)
 	pkt->trans = len;
 
 	usbhsf_tx_irq_ctrl(pipe, 0);
-	/* FIXME: Workaound for usb dmac that driver can be used in atomic */
-	if (usbhs_get_dparam(priv, has_usb_dmac)) {
-		usbhsf_dma_xfer_preparing(pkt);
-	} else {
-		INIT_WORK(&pkt->work, xfer_work);
-		schedule_work(&pkt->work);
-	}
+	INIT_WORK(&pkt->work, xfer_work);
+	schedule_work(&pkt->work);
 
 	return 0;
 
@@ -1034,7 +1023,8 @@ static int usbhsf_dma_try_pop(struct usbhs_pkt *pkt, int *is_done)
 
 	pkt->trans = len;
 
-	usbhsf_dma_xfer_preparing(pkt);
+	INIT_WORK(&pkt->work, xfer_work);
+	schedule_work(&pkt->work);
 
 	return 0;
 
