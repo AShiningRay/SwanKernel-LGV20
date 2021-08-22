fe42bb129858b1d66f4b5c69a98b57650059c3bc
PauloCesarMS <genesis45.ppc@gmail.com>
2021-08-22 16:49:37 -0300

    Remove some modules and adjust compilation.

--- CompileKernel.sh
+++ CompileKernel.sh
@@ -2,7 +2,7 @@ make O=KernelOut clean && make O=KernelOut mrproper
 make O=KernelOut ARCH=arm64 lineageos_h910_defconfig
 make -j$(nproc --all) O=KernelOut \
 		      KBUILD_BUILD_VERSION="SwanKernel" \
-                      ARCH=arm64 \
+                      ARCH=arm64 CONFIG_BUILD_ARM64_DT_OVERLAY=y DTC_EXT=dtc \
                       CLANG_TRIPLE=/home/paulo/Documentos/V20CustomKernel/CLANG/bin/aarch64-linux-gnu- \
                       CROSS_COMPILE=/home/paulo/Documentos/V20CustomKernel/toolchainaarch64/bin/aarch64-linux-android- \
 		      CROSS_COMPILE_ARM32=/home/paulo/Documentos/V20CustomKernel/toolchainarm32/bin/arm-linux-androideabi- \
