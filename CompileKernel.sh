e5b3f4530104e91dddcf39e3112f7a0d85155f62
PauloCesarMS <genesis45.ppc@gmail.com>
2021-08-22 23:29:43 -0300

    Added some configs about the IR, and changed the toolchain.

--- CompileKernel.sh
+++ CompileKernel.sh
@@ -4,6 +4,6 @@ make -j$(nproc --all) O=KernelOut \
 		      KBUILD_BUILD_VERSION="SwanKernel" \
                       ARCH=arm64 CONFIG_BUILD_ARM64_DT_OVERLAY=y DTC_EXT=dtc \
                       CLANG_TRIPLE=/home/paulo/Documentos/V20CustomKernel/CLANG/bin/aarch64-linux-gnu- \
-                      CROSS_COMPILE=/home/paulo/Documentos/V20CustomKernel/toolchainaarch64/bin/aarch64-linux-android- \
+                      CROSS_COMPILE=/home/paulo/Documentos/V20CustomKernel/toolchainaarch64/bin/aarch64-none-linux-gnu- \
 		      CROSS_COMPILE_ARM32=/home/paulo/Documentos/V20CustomKernel/toolchainarm32/bin/arm-linux-androideabi- \
 		      CC=/home/paulo/Documentos/V20CustomKernel/CLANG/bin/clang | tee KernelOut/swanKernel.log
