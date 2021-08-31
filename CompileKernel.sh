dd6e5b21c76ae79e9485c15477f806b9525b8abb
PauloCesarMS <genesis45.ppc@gmail.com>
2021-08-30 22:02:17 -0300

    Small changes to the compilation script.
    Preparing to do some explanations on how to compile it after i either fix, or find help about fixing the Bluetooth bug.

--- CompileKernel.sh
+++ CompileKernel.sh
@@ -3,7 +3,7 @@ make O=KernelOut ARCH=arm64 lineageos_h910_defconfig
 make -j$(nproc --all) O=KernelOut \
 		      KBUILD_BUILD_VERSION="SwanKernel" \
                       ARCH=arm64 CONFIG_BUILD_ARM64_DT_OVERLAY=y DTC_EXT=dtc \
-                      CLANG_TRIPLE=/home/paulo/Documentos/V20CustomKernel/CLANG/bin/aarch64-linux-gnu- \
-                      CROSS_COMPILE=/home/paulo/Documentos/V20CustomKernel/toolchainaarch64/bin/aarch64-none-linux-gnu- \
-		      CROSS_COMPILE_ARM32=/home/paulo/Documentos/V20CustomKernel/toolchainarm32/bin/arm-linux-androideabi- \
-		      CC=/home/paulo/Documentos/V20CustomKernel/CLANG/bin/clang | tee KernelOut/swanKernel.log
+                      CLANG_TRIPLE=$HOME/androidkerneltools/CLANG/bin/aarch64-linux-gnu- \
+                      CROSS_COMPILE=$HOME/androidkerneltools/toolchainaarch64/bin/aarch64-none-linux-gnu- \
+		      CROSS_COMPILE_ARM32=$HOME/androidkerneltools/toolchainarm32/bin/arm-linux-androideabi- \
+		      CC=$HOME/androidkerneltools/CLANG/bin/clang | tee KernelOut/swanKernel.log
