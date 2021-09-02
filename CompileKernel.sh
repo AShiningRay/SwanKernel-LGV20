fb1519fbacae7f29560b734afa582ad661f48e4e
PauloCesarMS <genesis45.ppc@gmail.com>
2021-09-01 22:43:06 -0300

    Update the compilation script for better readability.

--- CompileKernel.sh
+++ CompileKernel.sh
@@ -1,8 +1,11 @@
-make O=KernelOut clean && make O=KernelOut mrproper
-make O=KernelOut ARCH=arm64 lineageos_h910_defconfig
-make -j$(nproc --all) O=KernelOut \
+rm -rf KernelOut
+mkdir KernelOut
+make O=KernelOut/H910-H915 clean
+make O=KernelOut/H910-H915 mrproper
+make O=KernelOut/H910-H915 ARCH=arm64 lineageos_h910_defconfig
+make -j$(nproc --all) O=KernelOut/H910-H915 \
 		      KBUILD_BUILD_VERSION="SwanKernel" \
-                      ARCH=arm64 CONFIG_BUILD_ARM64_DT_OVERLAY=y DTC_EXT=dtc \
+                      ARCH=arm64 CONFIG_BUILD_ARM64_DT_OVERLAY=y DTC_EXT=/var/lib/snapd/snap/bin/dtc \
                       CLANG_TRIPLE=$HOME/androidkerneltools/CLANG/bin/aarch64-linux-gnu- \
                       CROSS_COMPILE=$HOME/androidkerneltools/toolchainaarch64/bin/aarch64-none-linux-gnu- \
 		      CROSS_COMPILE_ARM32=$HOME/androidkerneltools/toolchainarm32/bin/arm-linux-androideabi- \
